2f2c621e7e6eaff257244a817a01913af3d08911
James Morrison <james@frontastic.cloud>
2023-04-19 04:33:29 +0100

    Corrected NODE_ENV flag

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
-	const dev = options.env?.["ENV"] === "dev";
+	const dev = options.env?.["NODE_ENV"] === "dev";
 	return {
 		entry: dev ? ["src/index.ts"] : ["src"],
 		outDir: "lib",
